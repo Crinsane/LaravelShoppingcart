043e7fb1cef4a41dd5c4fd52bd2adb4d62d77157
Edwin <tkaw220@gmail.com>
2013-07-02 11:44:52 +0800

    Convert indentation to tabs and change line ending to Unix.
    Signed-off-by: Edwin <tkaw220@gmail.com>

--- src/Gloudemans/Shoppingcart/Facades/Cart.php
+++ src/Gloudemans/Shoppingcart/Facades/Cart.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Facade;
 
 class Cart extends Facade {
 
-    /**
-     * Get the registered name of the component.
-     *
-     * @return string
-     */
-    protected static function getFacadeAccessor() { return 'cart'; }
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'cart'; }
 
 }
--- src/Gloudemans/Shoppingcart/ShoppingcartServiceProvider.php
+++ src/Gloudemans/Shoppingcart/ShoppingcartServiceProvider.php
@@ -4,18 +4,18 @@ use Illuminate\Support\ServiceProvider;
 
 class ShoppingcartServiceProvider extends ServiceProvider {
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
 
-        $this->app['cart'] = $this->app->share(function($app)
-        {
-            $session = $app['session'];
-            return new Cart($session);
-        });
-    }
+		$this->app['cart'] = $this->app->share(function($app)
+		{
+			$session = $app['session'];
+			return new Cart($session);
+		});
+	}
 }
