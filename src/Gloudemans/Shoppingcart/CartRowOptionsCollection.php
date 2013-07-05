eb641093da732e74eafad1ebfa267936f0989911
Edwin <tkaw220@gmail.com>
2013-07-05 10:01:47 +0800

    Convert spaces to tabs.
    Signed-off-by: Edwin <tkaw220@gmail.com>

--- src/Gloudemans/Shoppingcart/CartRowOptionsCollection.php
+++ src/Gloudemans/Shoppingcart/CartRowOptionsCollection.php
@@ -4,31 +4,31 @@ use Illuminate\Support\Collection;
 
 class CartRowOptionsCollection extends Collection {
 
-    public function __construct($items)
-    {
-        parent::__construct($items);
-    }
+	public function __construct($items)
+	{
+		parent::__construct($items);
+	}
 
-    public function __get($arg)
-    {
-        if($this->has($arg))
-        {
-            return $this->get($arg);
-        }
+	public function __get($arg)
+	{
+		if($this->has($arg))
+		{
+			return $this->get($arg);
+		}
 
-        return NULL;
-    }
+		return NULL;
+	}
 
-    public function search(Array $search)
-    {
-        foreach($search as $key => $value)
-        {
-            $found = ($this->{$key} === $value) ? true : false;
+	public function search(Array $search)
+	{
+		foreach($search as $key => $value)
+		{
+			$found = ($this->{$key} === $value) ? true : false;
 
-            if( ! $found) return false;
-        }
+			if( ! $found) return false;
+		}
 
-        return $found;
-    }
+		return $found;
+	}
 
 }
