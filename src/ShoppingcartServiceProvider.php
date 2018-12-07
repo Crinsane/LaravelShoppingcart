0569ba505377ca8d86a95e5a67472f64962398be
Andrew Westrope <andrew@warwickshire.it>
2018-12-07 15:56:24 +1000

    Ensure the migration is only published once. Renamed migration to date it was generated.

--- src/ShoppingcartServiceProvider.php
+++ src/ShoppingcartServiceProvider.php
@@ -29,13 +29,8 @@ class ShoppingcartServiceProvider extends ServiceProvider
             }
         });
 
-        if ( ! class_exists('CreateShoppingcartTable')) {
-            // Publish the migration
-            $timestamp = date('Y_m_d_His', time());
-
-            $this->publishes([
-                __DIR__.'/../database/migrations/0000_00_00_000000_create_shoppingcart_table.php' => database_path('migrations/'.$timestamp.'_create_shoppingcart_table.php'),
-            ], 'migrations');
-        }
+        $this->publishes([
+            realpath(__DIR__.'/../databaase/migrations') => $this->app->databasePath().'/migrations'
+        ]);
     }
 }
