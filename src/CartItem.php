e83371d5429be751ad92e1a30a12d121a20f030a
Patrick Henninger <donnironon@hotmail.de>
2019-03-01 15:11:31 +0100

    Removed unneccessary return statements

--- src/CartItem.php
+++ src/CartItem.php
@@ -377,14 +377,10 @@ class CartItem implements Arrayable, Jsonable
                     return with(new $this->associatedModel())->find($this->id);
                 }
 
-                return;
-
             case 'modelFQCN':
                 if (isset($this->associatedModel)) {
                     return $this->associatedModel;
                 }
-
-                return;
 
             default:
                 return;
