fe3e62000942fe76f9eb1f34eb629daf3a1129b5
cobraprojects <42706279+cobraprojects@users.noreply.github.com>
2019-09-30 13:25:36 +0200

    Update Cart.php

--- src/Cart.php
+++ src/Cart.php
@@ -358,7 +358,7 @@ class Cart
             'identifier' => $identifier,
             'instance' => $this->currentInstance(),
             'content' => serialize($content),
-            'created_at' => date('Y-m-d H:i:s');
+            'created_at' => date('Y-m-d H:i:s')
         ]);
 
         $this->events->fire('cart.stored');
