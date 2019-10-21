f31c153e3e6a4898b9498d412be39086086d2e3b
Cristian <sartoricgm@gmail.com>
2019-10-21 19:00:34 +0200

    Update CartItem.php
    weight variable in if block

--- src/CartItem.php
+++ src/CartItem.php
@@ -98,7 +98,7 @@ class CartItem implements Arrayable, Jsonable
         if (strlen($price) < 0 || !is_numeric($price)) {
             throw new \InvalidArgumentException('Please supply a valid price.');
         }
-        if (strlen($price) < 0 || !is_numeric($weight)) {
+        if (strlen($weight) < 0 || !is_numeric($weight)) {
             throw new \InvalidArgumentException('Please supply a valid weight.');
         }
 
