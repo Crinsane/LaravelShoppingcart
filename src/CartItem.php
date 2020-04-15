00e180b6cb968d6453426fdc7bd6475fcea2335c
Harinder <harinderdivine@gmail.com>
2020-04-15 12:41:21 +0530

    Update CartItem.php

--- src/CartItem.php
+++ src/CartItem.php
@@ -384,9 +384,11 @@ class CartItem implements Arrayable, Jsonable
      */
     public function toJson($options = 0)
     {
-        if(isset($this->associatedModel){
+        if (isset($this->associatedModel)){
+            
            return json_encode(array_merge($this->toArray(), ['model' => $this->model]), $options);
         }
+        
         return json_encode($this->toArray(), $options);
     }
 
