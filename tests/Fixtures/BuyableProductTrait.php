80631bab397b49af7c6d449fd2c3f2c930211bee
Patrick <privat@skyraptor.eu>
2021-01-18 21:48:35 +0100

    Update BuyableProductTrait.php

--- tests/Fixtures/BuyableProductTrait.php
+++ tests/Fixtures/BuyableProductTrait.php
@@ -8,39 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class BuyableProductTrait extends Model implements Buyable
 {
     use \Gloudemans\Shoppingcart\CanBeBought;
-
-    /**
-     * @var int|string
-     */
-    private $id;
-
-    /**
-     * @var string
-     */
-    private $name;
-
-    /**
-     * @var float
-     */
-    private $price;
-
-    /**
-     * @var float
-     */
-    private $weight;
-
-    /**
-     * BuyableProduct constructor.
-     *
-     * @param int|string $id
-     * @param string     $name
-     * @param float      $price
-     */
-    public function __construct($id = 1, $name = 'Item name', $price = 10.00, $weight = 0)
-    {
-        $this->id = $id;
-        $this->name = $name;
-        $this->price = $price;
-        $this->weight = $weight;
-    }
 }
