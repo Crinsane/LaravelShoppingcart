4a60b7cda170c7e0c0c79950b4632e6d45f5c715
Patrick <privat@skyraptor.eu>
2021-01-18 21:52:18 +0100

    Update BuyableProductTrait.php

--- tests/Fixtures/BuyableProductTrait.php
+++ tests/Fixtures/BuyableProductTrait.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class BuyableProductTrait extends Model implements Buyable
 {
     use \Gloudemans\Shoppingcart\CanBeBought;
+    
+    protected $attributes = [
+        'id'  => 1,
+        'name' => 'Item name',
+        'price' => 10.00,
+        'weight' => 0,
+    ];
 }
